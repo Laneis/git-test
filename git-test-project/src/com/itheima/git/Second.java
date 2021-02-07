f729ce095b6232cc8b94aaa278af0731c2cff67a
tom_glb <goodMorning_glb@atguigu.com>
2021-02-07 13:20:47 +0800

    将本地工程添加至git

---
+++ git-test-project/src/com/itheima/git/Second.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+/**
+ * @author shkstart
+ * @create 2021-02-0713:19
+ */
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("hello second");
+    }
+}
