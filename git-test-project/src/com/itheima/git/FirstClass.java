8a7cba88b5ba3a026523bfc54ac90d796cc28648
tom_glb <goodMorning_glb@atguigu.com>
2021-02-07 11:19:09 +0800

    将本地工程添加至git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @author shkstart
+ * @create 2021-02-0710:55
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
